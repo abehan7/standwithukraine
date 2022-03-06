9948413fb207fb8b4c4ee21f5db3bbed638c1b25
한준규 <hanjk13262@gmail.com>
2022-03-07 01:18:51 +0900

    fix

--- src/router/PostDetail.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-const PostDetailEl = styled.div``;
-const PostDetail = () => {
-  return <PostDetailEl></PostDetailEl>;
-};
-
-export default PostDetail;
